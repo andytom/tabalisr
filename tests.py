f848c9313bb2165e7f9bd7fa429d406866fe9a8c
Thomas O'Donnell <andy.tom@gmail.com>
2013-05-12 16:24:27 +0000

    Rewritting so that each test is an individual test.

--- tests.py
+++ tests.py
@@ -6,19 +6,33 @@ from tabalisr.lib import process_string
 
 
 #-----------------------------------------------------------------------------#
+# Helper functions
+#-----------------------------------------------------------------------------#
 def load_json_tests(filename):
     with open(filename, 'r') as f:
         return json.loads(f.read())
 
 
 #-----------------------------------------------------------------------------#
+# Test Cases
+#-----------------------------------------------------------------------------#
 class LibTestCase(unittest.TestCase):
-    def test_table_construction(self):
+    """Tests to test the libary functions"""
+    def setUp(self):
         json_file = 'test/table_construction_tests.json'
-        for test in load_json_tests(json_file):
-            assert process_string(test['input']) == test['result']
+        self.all_tests = load_json_tests(json_file)
+
+    def _run_test(self, test_name):
+        test = self.all_tests[test_name]
+        assert process_string(test['input']) == test['result']
+
+    def test_table_construction(self):
+        self._run_test("Basic Construction test")
+
+    def test_whitespace_in_string(self):
+        self._run_test("Whitespace Test")
 
 
 #-----------------------------------------------------------------------------#
 if __name__ == '__main__':
-        unittest.main()
+    unittest.main()
